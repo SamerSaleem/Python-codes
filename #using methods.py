e94a62da767831ef113a667c554222a291f6d823
Samer Rafid <Samer.Rafid@gmail.com>
2022-11-03 21:22:44 +0300

    method with Arya.

--- #using methods.py
+++ #using methods.py
@@ -3,11 +3,15 @@ mycolors = ['red', 'blue'] #the list of strings
 
 mycolors.append('black') #using method of append to add new item to list.
 
-print (mycolors)
+print ('this is my first print', mycolors)
 
 mycolors.insert(1, 'purple') #using insert we can also decide location of the added value, for example location 1.
 
-print(mycolors)
+print('this is my 2nd print', mycolors)
 
 mycolors.reverse()  #using reverse to print the list backwards.
-print(mycolors)
+print('this is my last print', mycolors)
+
+mycolors.clear()
+print('Another method with my son Arya', mycolors)
+
