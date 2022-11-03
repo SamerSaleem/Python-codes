9a642d5d580034a9bfac8cc7d905049fea6b7839
Samer Rafid <Samer.Rafid@gmail.com>
2022-11-03 21:40:38 +0300

    ...

--- #using methods.py
+++ #using methods.py
@@ -1,11 +1,12 @@
-#using methods
+#how to use methods
 mycolors = ['red', 'blue'] #the list of strings
 
 mycolors.append('black') #using method of append to add new item to list.
 
 print ('this is my first print', mycolors)
 
-mycolors.insert(1, 'purple') #using insert we can also decide location of the added value, for example location 1.
+mycolors.insert(1, 'purple') #using insert we can also decide 
+#location of the added value, for example location 1.
 
 print('this is my 2nd print', mycolors)
 
