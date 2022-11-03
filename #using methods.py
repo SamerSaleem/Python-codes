3b1daa10ab44af6b8c19d31679997b770692a5cd
Samer Rafid <Samer.Rafid@gmail.com>
2022-11-03 21:09:17 +0300

    using reverse in methods..

--- #using methods.py
+++ #using methods.py
@@ -8,3 +8,6 @@ print (mycolors)
 mycolors.insert(1, 'purple') #using insert we can also decide location of the added value, for example location 1.
 
 print(mycolors)
+
+mycolors.reverse()  #using reverse to print the list backwards.
+print(mycolors)
