4a38614932c5371ebfe7cb5b88f096a8c5877b34
Samer Rafid <Samer.Rafid@gmail.com>
2022-11-03 21:07:57 +0300

    using insert.

--- #using methods.py
+++ #using methods.py
@@ -4,3 +4,7 @@ mycolors = ['red', 'blue'] #the list of strings
 mycolors.append('black') #using method of append to add new item to list.
 
 print (mycolors)
+
+mycolors.insert(1, 'purple') #using insert we can also decide location of the added value, for example location 1.
+
+print(mycolors)
