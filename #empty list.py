13ebf437e76e500591fa6c8ac7f8dae05ee9aac4
Samer Rafid <Samer.Rafid@gmail.com>
2022-11-04 17:25:15 +0300

    uploading.

---
+++ #empty list.py
@@ -0,0 +1,8 @@
+#empty list
+
+my_list = []  # Creating an empty list.
+
+for i in range(5):
+    my_list.insert(0, i + 1) #this will make code add new numbers to position 0, this is why all numbers reversed.
+
+print(my_list)
